8887f610ee0890a3a870a8cbcc6723ba349a3129
raxracks <39295557+raxracks@users.noreply.github.com>
2022-03-09 14:41:45 +1300

    Update script.js

--- demo/script.js
+++ demo/script.js
@@ -1,3 +1,11 @@
 if(!window.matchMedia("only screen and (max-width: 760px)").matches) {
   document.location.href = "/widescreen.html";
 }
+
+window.addEventListener("load", e => {
+  let test_switch = document.getElementById("test-switch");
+  
+  test_switch.addEventListener("change", () => {
+    document.getElementById("switch-state").innerText = "switch is " + (test_switch.checked ? "on" : "off");
+  });
+});
